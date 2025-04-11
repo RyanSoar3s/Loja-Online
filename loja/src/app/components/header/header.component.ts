9db3254564cd0cfb339b62ec138ec29a2aa63ab5
RyanSoar3s <ryansoares.dev@gmail.com>
2025-04-11 12:43:19 -0300

    feat: add user option

--- loja/src/app/components/header/header.component.ts
+++ loja/src/app/components/header/header.component.ts
@@ -1,7 +1,19 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  OnInit,
+  OnDestroy
+
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faShoppingCart, faMagnifyingGlass, faXmark } from '@fortawesome/free-solid-svg-icons';
+import {
+  faShoppingCart,
+  faMagnifyingGlass,
+  faXmark,
+  faUser
+
+} from '@fortawesome/free-solid-svg-icons';
 
 import { ResponsiveService } from '../../services/responsive.service';
 import { Subscription } from 'rxjs';
@@ -18,9 +30,13 @@ import { Subscription } from 'rxjs';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit, OnDestroy {
+  @HostBinding("style.--width-search-bar-header") width_search_bar_style!: string;
+  @HostBinding("style.--font-size-h1-header") font_size_h1_style!: string;
+
   protected faShoppingCart = faShoppingCart;
   protected faMagnifyingGlass = faMagnifyingGlass;
   protected faXmark = faXmark;
+  protected faUser = faUser;
 
   private readonly XSMALL = '(max-width: 599px)';
   private readonly SMALL = '(min-width: 600px) and (max-width: 749px)';
@@ -43,7 +59,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
       this.showCloseButton = false;
 
       if (state.breakpoints[this.XSMALL]) {
-        this.width_search_bar = "50%";
+        this.width_search_bar = "40%";
         this.font_size_h1 = "1.3em";
 
       }
@@ -68,8 +84,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
       }
 
-      document.documentElement.style.setProperty("--width-search-bar-header", this.width_search_bar);
-      document.documentElement.style.setProperty("--font-size-h1-header", this.font_size_h1);
+      this.width_search_bar_style = this.width_search_bar;
+      this.font_size_h1_style = this.font_size_h1;
 
     })
 
@@ -79,7 +95,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
     if (!this.showSearchBar) {
       this.showSearchBar = !this.showSearchBar;
       this.showCloseButton = !this.showCloseButton;
-      document.documentElement.style.setProperty("--width-search-bar-header", "85%");
+      this.width_search_bar_style = "87%";
 
     }
 
@@ -89,7 +105,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
     if (this.showSearchBar) {
       this.showSearchBar = !this.showSearchBar;
       this.showCloseButton = !this.showCloseButton;
-      document.documentElement.style.setProperty("--width-search-bar-header", this.width_search_bar);
+      this.width_search_bar_style = this.width_search_bar;
 
     }
 
