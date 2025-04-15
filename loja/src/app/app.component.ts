2ce7e2a5570212cdef580c59fa2c2ec0c18f2ee0
RyanSoar3s <ryansoares.dev@gmail.com>
2025-04-15 12:06:32 -0300

    feat: add main component

--- loja/src/app/app.component.ts
+++ loja/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    MainContentComponent
 
   ],
   templateUrl: './app.component.html',
