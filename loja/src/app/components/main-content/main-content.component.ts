dfa2f3ff31ee8950d912be39dfebb6b6d0a5461f
RyanSoar3s <ryansoares.dev@gmail.com>
2025-04-15 12:24:23 -0300

    chore: add banner and responsive options

--- loja/src/app/components/main-content/main-content.component.ts
+++ loja/src/app/components/main-content/main-content.component.ts
@@ -3,9 +3,12 @@ import {
   Component,
   AfterViewInit,
   ViewChild,
+  OnInit,
   ViewChildren,
+  OnDestroy,
   ElementRef,
-  Renderer2
+  Renderer2,
+  HostBinding
 
 } from '@angular/core';
 
@@ -19,6 +22,9 @@ import {
 
 } from '@fortawesome/free-solid-svg-icons';
 
+import { ResponsiveService } from '../../services/responsive.service';
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-main-content',
   imports: [
@@ -29,9 +35,15 @@ import {
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
-export class MainContentComponent implements AfterViewInit {
+export class MainContentComponent implements OnInit, AfterViewInit, OnDestroy {
+  @HostBinding("style.--width-ul-container-categories-promos-main") width_ul_container_categories_promos_style!: string;
   @ViewChild("carousel") carousel!: ElementRef;
   @ViewChildren("radio", { read: ElementRef }) radio!: Array<ElementRef>;
+
+  private readonly XSMALL = '(max-width: 599px)';
+  private readonly SMALL = '(min-width: 600px) and (max-width: 749px)';
+  private readonly MEDIUM = '(min-width: 750px) and (max-width: 969px)';
+  private readonly LARGE = '(min-width: 970px) and (max-width: 1199px)';
 
   protected faDollar = faDollar;
   protected faTicket = faTicket;
@@ -40,11 +52,53 @@ export class MainContentComponent implements AfterViewInit {
   protected faShop = faShop;
 
   private posX: number = 1;
+  private sub!: Subscription
 
-  constructor(private renderer: Renderer2) {}
+  constructor(
+    private renderer: Renderer2,
+    private responsive$: ResponsiveService
+
+  ) {}
+  ngOnInit(): void {
+    this.sub = this.responsive$.onBreakpointChange().subscribe((state) => {
+      if (state.breakpoints[this.XSMALL]) {
+        this.width_ul_container_categories_promos_style = "265px";
+
+      }
+      else if (state.breakpoints[this.SMALL]) {
+        this.width_ul_container_categories_promos_style = "410px";
+
+
+      }
+      else if (state.breakpoints[this.MEDIUM]) {
+        this.width_ul_container_categories_promos_style = "410px";
+
+      }
+      else if (state.breakpoints[this.LARGE]) {
+        this.width_ul_container_categories_promos_style = "680px";
+
+      }
+      else {
+        this.width_ul_container_categories_promos_style = "680px";
+
+      }
+
+
+
+    })
+
+  }
 
   ngAfterViewInit(): void {
     this.scroollCarousel();
+
+  }
+
+  ngOnDestroy(): void {
+    if (this.sub) {
+      this.sub.unsubscribe();
+
+    }
 
   }
 
