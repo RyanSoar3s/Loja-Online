02fac8148920f4be44dbe2e715837577401e2a43
RyanSoar3s <ryansoares.dev@gmail.com>
2025-05-19 12:31:21 -0300

    feat: add footer component

--- loja/src/app/app.component.ts
+++ loja/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '@components/header/header.component';
 import { MainContentComponent } from '@components/main-content/main-content.component';
+import { FooterComponent } from '@components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HeaderComponent,
-    MainContentComponent
+    MainContentComponent,
+    FooterComponent
 
   ],
   templateUrl: './app.component.html',
