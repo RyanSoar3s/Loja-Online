1ee36d8591950fc3e9b313a5469b1161923984ce
RyanSoar3s <ryansoares.dev@gmail.com>
2025-05-27 20:17:01 -0300

    feat(AppComponent): implement API integration to fetch and set products

--- loja/src/app/app.component.ts
+++ loja/src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@components/header/header.component';
 import { FooterComponent } from '@components/footer/footer.component';
+import { Product } from '@models/products.model';
+import { ApiService } from '@services/api.service';
+import { ProductsService } from '@services/products.service';
 
 @Component({
   selector: 'app-root',
@@ -17,4 +20,19 @@ import { FooterComponent } from '@components/footer/footer.component';
 })
 export class AppComponent implements OnInit {
   title = 'loja';
+
+  constructor(
+    private api: ApiService,
+    private productsService: ProductsService
+
+  ) {}
+
+  ngOnInit(): void {
+    this.api.requestApi().subscribe((data: Array<Product>) => {
+      this.productsService.setProducts(data);
+
+    });
+
+  }
+
 }
