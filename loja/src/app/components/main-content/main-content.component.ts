b9cb3f6d8f26a3a91e3708be4716ab36678815d9
RyanSoar3s <ryansoares.dev@gmail.com>
2025-05-27 20:08:12 -0300

    refactor(MainContent): update product area component

--- loja/src/app/components/main-content/main-content.component.ts
+++ loja/src/app/components/main-content/main-content.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 import { PromotionComponent } from '@components/promotion/promotion.component';
 import { CategoriesComponent } from '@components/categories/categories.component';
-import { ProductsComponent } from '@components/products/products.component';
+import { ProductAreaComponent } from '@components/product-area/product-area.component';
 
 @Component({
   selector: 'app-main-content',
   imports: [
     PromotionComponent,
     CategoriesComponent,
-    ProductsComponent
+    ProductAreaComponent
 
   ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
 export class MainContentComponent {
-
-
+  
 }
