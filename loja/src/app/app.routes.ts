66764309f22417fb1d1ea1582ccfce686724eed3
RyanSoar3s <ryansoares.dev@gmail.com>
2025-05-27 19:47:02 -0300

    feat(routes): configure routes for application

--- loja/src/app/app.routes.ts
+++ loja/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { ProductViewerComponent } from '@components/product-viewer/product-viewer.component';
+import { PageNotFoundComponent } from '@components/page-not-found/page-not-found.component';
+import { MainContentComponent } from '@components/main-content/main-content.component';
+import { SearchProductsComponent } from '@components/search-products/search-products.component';
+import { ProductsComponent } from '@components/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: MainContentComponent },
+  {
+    path: "produtos",
+    component: SearchProductsComponent,
+    children: [
+      { path: "", component: ProductsComponent },
+      { path: "produto", component: ProductViewerComponent },
+      { path: "**", redirectTo: "nao-encontrado/404" }
+
+    ]
+
+  },
+  { path: "nao-encontrado/404", component: PageNotFoundComponent },
+  { path: "**", redirectTo: "nao-encontrado/404" }
+
+];
